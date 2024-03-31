72cd32daf6fc6035602a7b5253d0dd66ef8a32f0
vadrsa <davitasryan3@gmail.com>
2024-03-31 16:49:05 +0400

    Dummy commit

--- test/PingPong.Server.Tests/Helpers/WaitHelpers.cs
+++ test/PingPong.Server.Tests/Helpers/WaitHelpers.cs
@@ -9,6 +9,7 @@ public static class WaitHelpers
         var sw = Stopwatch.StartNew();
         while (!condition() && sw.ElapsedMilliseconds < timeoutMs)
         {
+            
             await Task.Delay(100);
         }
     }
